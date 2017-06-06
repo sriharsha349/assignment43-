5622e9a6b32d7cb2dadb4f466bf404a2de9ad460
sri Harsha <sri Harsha>
2017-06-06 12:52:32 -0500

    initial comm

--- src/main/java/com/harsha/spring/SpringSampleApplication.java
+++ src/main/java/com/harsha/spring/SpringSampleApplication.java
@@ -1,10 +1,21 @@
 package com.harsha.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringSampleApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	String home (){
+		return "Hello world";
+}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSampleApplication.class, args);
